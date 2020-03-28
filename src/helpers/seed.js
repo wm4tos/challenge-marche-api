fd058a0246656bfda9c956e95f5ae69870df704a
Wesley Matos <wesley.matos@redfox.tech>
2020-03-28 17:07:44 -0300

    seed orders

--- src/helpers/seed.js
+++ src/helpers/seed.js
@@ -1,4 +1,9 @@
+const path = require('path');
+
+const csv = require('./csv');
 const User = require('../modules/users/model');
+const Order = require('../modules/orders/model');
+
 const usersMock = require('../lib/users');
 
 const seed = force => async (mock, model) => {
@@ -13,12 +18,51 @@ const seed = force => async (mock, model) => {
   return true;
 };
 
+const getOrdersMock = async () => {
+  const mockPath = path.join(__dirname, '..', 'lib', 'orders.csv');
+
+  const fromStringToBool = item => /TRUE/.test(item);
+  const param = {
+    colParser: {
+      hasColdProducts: fromStringToBool,
+      hasFruitsOrVegetables: fromStringToBool,
+    },
+  };
+
+  const preFileLine = (lineData, lineNumber) => {
+    if (lineNumber === 0) {
+      const arr = lineData.split(',');
+
+      return arr.map((x) => {
+        const matches = x.match(/_\w/g);
+
+        return matches.reduce(
+          (acc, match) => acc.replace(match, match.substring(1).toUpperCase()),
+          x,
+        );
+      });
+    }
+
+    return lineData;
+  };
+
+  return csv(
+    mockPath,
+    param,
+    preFileLine,
+  );
+};
+
 const seedUsers = fn => fn(usersMock, User);
+
+const seedOrders = async fn => fn(await getOrdersMock(), Order);
 
 module.exports = async (force) => {
   const toSeed = seed(force);
   const u = await seedUsers(toSeed);
+  const o = await seedOrders(toSeed);
 
   if (u) process.stdout.write('Users seeded! :)\n');
+  if (o) process.stdout.write('Orders seeded! :)\n');
   return null;
 };
