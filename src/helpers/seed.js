e1ae6f1d24dc2a883679803ddb4c07c72d27e058
Wesley Matos <wesley.matos@redfox.tech>
2020-04-14 21:31:42 -0300

    use functions with single responsability

--- src/helpers/seed.js
+++ src/helpers/seed.js
@@ -1,21 +1,27 @@
 const User = require('../modules/users/model');
+
 const usersMock = require('../lib/users');
 
-const seedUsers = force => new Promise(async (resolve) => {
-  if (force) await User.deleteMany();
+const seed = force => async (mock, model) => {
+  if (force) await model.deleteMany();
 
-  const users = await User.find();
+  const length = await model.countDocuments();
 
-  if (users.length) return resolve(false);
+  if (length > 0) return false;
 
-  await User.insertMany(usersMock);
+  await model.insertMany(mock);
 
-  return resolve(true);
-});
+  return true;
+};
+
+const seedUsers = seeder => seeder(usersMock, User);
 
 module.exports = async (force) => {
-  const u = await seedUsers(force);
+  const seeder = seed(force);
+  const u = await seedUsers(seeder);
 
-  if (u) return process.stdout.write('Database seeded! :)\n');
+  if (u) process.stdout.write('Users seeded! :)\n');
+  else process.stdout.write('Users collection already has data! :)\n');
+
   return null;
 };
