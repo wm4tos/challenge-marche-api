92c4967b66e724ff59f2b896dd78de79db8d01ba
Wesley Matos <wrickee@gmail.com>
2020-04-17 03:06:30 -0300

    implement repository methods on service

--- src/modules/files/service.js
+++ src/modules/files/service.js
@@ -1,3 +1,7 @@
-const { saveFileOnRedis } = require('./repository');
+const { saveFileOnRedis, getFileFromRedis, deleteFileFromRedis } = require('./repository');
 
 module.exports.saveInRedis = saveFileOnRedis;
+
+module.exports.getFromRedis = getFileFromRedis;
+
+module.exports.deleteFromRedis = deleteFileFromRedis;
